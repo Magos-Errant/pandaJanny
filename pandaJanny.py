5749f42660d868d45f0b8c14b08e148e780ee102
sulejman3 <30478613+sulejman3@users.noreply.github.com>
2021-01-19 02:05:08 +0000

    small fix
    elements of the regexp should now be a bit more readable

--- pandaJanny.py
+++ pandaJanny.py
@@ -4,12 +4,15 @@ from pathlib import Path
 import os
 import re
 
-# Expected metadata
-# (\(comiket\))? \[circle \(artist\)\])? title (series) [language] [translator]
-
-doujinPattern = re.compile(
-        r'(\(.*\))? (\[.*\])? (.*) (\(.*\))? (\[.*\])? (\[.*\])?'
-        ) # this regexp sucks btw
+# The regexp for extracting metadata
+doujinPattern = re.compile(r'''
+    (\(.*\))? # when it was released
+    (\[.*\])? # circle name (with artist)
+    (.*) # title
+    (\(.*\))? # series this is based upon
+    (\[.*\])? # language
+    (\[.*\])? # translation group
+    ''', re.VERBOSE) # this regexp sucks btw
 
 print('Hello! Point me to the location of all your sadpandas')
 while True:
@@ -22,15 +25,15 @@ while True:
     break
 
 for doujinDir in Path.listdir(workingDir):
-    
+    print(' - ' + str(doujinDir))
     doujinInfo = doujinPattern.search(str(doujinDir)) # get the metadata with regex from directory name
-    
     os.chdir(doujinDir)
 
     rawTags = open('rawTags','w')
     # TODO maybe do some editing on the search results
     # TODO write each group as separate line in the file, prefixed with correct namespace
     rawTags.close()
-
     os.chdir('..')
+    print('done')
+    
 print('All done, thank you for waiting')
